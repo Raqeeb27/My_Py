1cb05c35421301fc040b03978fcb2d279fa0c342
Mohammed Abdul Raqeeb <raqeeb2709@gmail.com>
2022-11-09 00:31:19 +0530

    commit

--- WIN10.py
+++ WIN10.py
@@ -1,6 +1,7 @@
 import random
-
 import win10toast
+
 a=["Assalamualaikum","Mohammed","Abdul","Raqeeb","Arshad","sahab"]
 b=win10toast.ToastNotifier()
-b.show_toast(random.choice(a),random.choice(a),None,1)
+b.show_toast(random.choice(a),random.choice(a),icon_path="C:\\Users\Abdul\\PycharmProjects\\My_Py\\car.ico"
+             ,duration=1.5)
