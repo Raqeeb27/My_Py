bbe7e84e5b5205d29b8b9cc0a2b402ebc90cc72f
Mohammed Abdul Raqeeb <raqeeb2709@gmail.com>
2023-06-17 13:57:37 +0530

    Poshished

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,25 +1,35 @@
--while True:
-    terms = int(input("\nEnter no.of terms in fibonacci sequence: "))
+while True:
+    try:
+        terms = int(input("\nEnter desired number of terms of Fibonacci sequence: "))
+    except ValueError:
+        print("\nInvalid Input\nPlease enter Positive Integer\n")
+        continue
+
     seq = []
     n1, n2 = 0, 1
+
     if terms <= 0:
-        print("Please Enter positive number")
+        print("\nPlease enter Positive Integer")
     elif terms == 1:
-        print("Fibonacci sequence upto 1 term is:", [n1])
+        print(f"\nFibonacci sequence upto 1 term is:\n[{n1}]")
     else:
-        print("The fibonacci sequence upto",terms , "terms is:")
+        print(f"\nThe Fibonacci sequence upto {terms} terms is:")
         for i in range(terms):
             seq.append(n1)
             n = n1 + n2
             n1 = n2
             n2 = n
         print(seq)
-    ch = input("\nDo you want to continue:(Y/N)\n")
-    if ch == "y" or ch == "Y" or ch == "yes" or ch == "YES" or ch == "Yes":
+
+    ch = input("\nDo you want to continue:(Y/N): ").upper()
+
+    if ch == "Y" or ch == "YES" :
+        print()
         continue
-    elif ch == "no" or ch == "NO" or ch == "No" or ch == "N" or ch == "n":
+    elif ch == "N" or ch == "NO" :
         break
     else:
-        print("Inalid input\n")
+        print("Invalid Input\n")
         exit(0)
+
 print("THANK YOU!!")
