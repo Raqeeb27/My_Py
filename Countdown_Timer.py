d9ea71cb087855163a390d14c383366ac3b8f193
Mohammed Abdul Raqeeb <raqeeb2709@gmail.com>
2025-08-22 05:03:43 +0530

    feat(countdown): Use colorama to enhance the output

--- Countdown_Timer.py
+++ Countdown_Timer.py
@@ -1,18 +1,28 @@
 import sys
-import pyfiglet
 from time import sleep
+from colorama import Fore, Style
+from pyfiglet import figlet_format
+
+
+CYAN, LIGHT_CYAN = Fore.CYAN, Fore.LIGHTCYAN_EX
+GREEN, LIGHT_GREEN = Fore.GREEN, Fore.LIGHTGREEN_EX
+BLUE = Fore.BLUE
+RED = Fore.RED
+YELLOW = Fore.YELLOW
+DIM, BRIGHT = Style.DIM, Style.BRIGHT
+RESET = Style.RESET_ALL
 
 
 def countdown(timer):
-    print()
+    print(f"{YELLOW}")
     last_lines = 0
     while timer >= 0:
         hours, remainder = divmod(timer, 3600)
         minutes, seconds = divmod(remainder, 60)
-        display_time = f'{hours:02d}:{minutes:02d}:{seconds:02d}'
+        display_time = f'{hours:02d} : {minutes:02d} : {seconds:02d}'
 
         # Render figlet text
-        big_text = pyfiglet.figlet_format(display_time, font="big")
+        big_text = figlet_format(display_time, font="big")
         lines = big_text.splitlines()
 
         # Move cursor up to overwrite previous lines
@@ -31,19 +41,28 @@ def countdown(timer):
         sleep(1)
 
 
-print("\n" + " Countdown ".center(19, "-"))
-input_time = int(input("\nEnter the time in seconds: "))
-if input_time <= 0:
-    print("\nEnter Positive integer!\n")
-    exit(1)
-input("\n --> Press Enter to start the timer: ")
-try:
-    countdown(input_time)
-except (ValueError, OSError) as e:
-    print(f"\n\nError: {e}")
-    exit(1)
-except (KeyboardInterrupt, EOFError):
-    print("\n\nKeyboard Interrupt!!!\nExiting...\n")
-    exit(1)
+print(f"\n{BRIGHT}{YELLOW} " + " Countdown ".center(19, "-")+ f"")
 
-print('!!! Times Up !!!\n')
+try:
+    input_time = int(input(f"\n{LIGHT_CYAN}Enter the time in seconds: "))
+    if input_time <= 0:
+        print(f"\n{RED}Invalid input!\n{BLUE}Please enter a positive integer.\n")
+        sys.exit(1)
+except ValueError:
+    print(f"\n{RED}Invalid input!\n{BLUE}Please enter a positive integer.\n")
+    sys.exit(1)
+except (KeyboardInterrupt, EOFError):
+    print(f"\n\n{RED}Keyboard Interrupt!!!\n{BLUE}Exiting...\n")
+    sys.exit(1)
+
+try:
+    input(f"\n{BLUE} --> Press Enter to start the timer when ready: ")
+    countdown(input_time)
+except (KeyboardInterrupt, EOFError):
+    print(f"\n\n{RED}Keyboard Interrupt!!!\n{BLUE}Exiting...\n")
+    sys.exit(1)
+except Exception as e:
+    print(f"\n\n{RED}Error: {e}")
+    sys.exit(1)
+
+print(f"{CYAN}  !!! Times Up !!!{RESET}\n")
