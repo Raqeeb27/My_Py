836137459d3c82a2048ddba25b085a921c7f3112
Mohammed Abdul Raqeeb <117599550+Raqeeb27@users.noreply.github.com>
2025-08-26 00:55:15 +0530

    feat(sleep): Implement safe top right corner click & windows 10 support

--- sleep computer.py
+++ sleep computer.py
@@ -1,8 +1,26 @@
-from pyautogui import hotkey, press
 from time import sleep
-hotkey("win","d")
-hotkey("alt","f4")
+from platform import system, release
+
+try:
+    from pyautogui import click, hotkey, press, size
+except ImportError:
+    print("This script requires the 'pyautogui' module.\nPlease install it using 'pip install pyautogui' and try again.")
+    exit(1)
+
+
+# Get screen width and height
+screen_width, screen_height = size()
+
+# Click at the rightmost top safe corner
+click(x=screen_width-2, y=1, clicks=1, interval=0, button='left')
+
+hotkey("alt", "f4")
 sleep(0.3)
 press("up")
-sleep(0.5)
+
+# Only press "up" if running on Windows 10
+if system() == "Windows" and release() == "10":
+    press("up")
+
+sleep(0.3)
 press("enter")
